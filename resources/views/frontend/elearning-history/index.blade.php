459d129f0ecd5b8e2a70ecbb2144ad28d3d2ea4f
unisexx <unisexx@gmail.com>
2024-07-02 10:03:29 +0700

    updated

--- resources/views/frontend/elearning-history/index.blade.php
+++ resources/views/frontend/elearning-history/index.blade.php
@@ -3,15 +3,15 @@
 @section('page', 'ระบบการเรียนรู้ออนไลน์ e-learning')
 
 @section('content')
-    <div class="container">        
+    <div class="container">
         <div class="row g-4 justify-content-between mb-5 wow fadeInDown">
             <div class="col-lg-6">
                 <div class="position-relative pb-2 my-4">
-                    <img src="http://localhost/html/images/icon-hat.svg" alt="" class="icon-hat">
+                    <img src="{{ asset('html/images/icon-hat.svg') }}" alt="" class="icon-hat">
                     <div class="title-send-us">ข้อมูลและประวัติการเรียน</div>
                 </div>
-            </div>            
-        </div>        
+            </div>
+        </div>
 
         <div class="row g-4 justify-content-center wow fadeInDown" style="visibility: visible; animation-name: fadeInDown;">
             <div class="col-12">
@@ -19,59 +19,59 @@
                     <thead>
                         <tr>
                             <th width="50">ลำดับ</th>
-                            <th>หลักสูตร</th>                                                        
+                            <th>หลักสูตร</th>
                             <th width="150">สถานะ</th>
                             <th width="200">วันที่ผ่านการทดสอบ</th>
                             <th></th>
                         </tr>
                     </thead>
                     <tbody>
-                        @forelse($exam_history as $key=>$item)            
-                        <tr>
-                            <td>{{ $key+1 }}</td>
-                            <td>{{ $item->curriculum->name }}</td>
-                            <td class="text-center">
-                                @if(!$item->post_date_finished)
-                                    <span class="text-default">ยังไม่จบบทเรียน</span>
-                                @elseif($item->post_pass_status == 'y')
-                                    <span class="text-success">ผ่าน</span>
-                                @else
-                                    <span class="text-danger">ไม่ผ่าน</span>
-                                @endif
-                            </td>
-                            <td>
-                                {{ DbToDate($item->post_date_finished, true, true) }}
-                            </td>
-                            <td class="text-center">
-                                @if($item->post_pass_status == 'y')
-                                    <a href="{{ url('certificate/pdf/'.$item->curriculum_id)}}" class="btn btn-success"><i class="fa fa-download"></i> ดาวน์โหลดใบประกาศ</a>
-                                @else
-                                    <a href="{{ url('elearning/curriculum/'.$item->curriculum_id) }}" class="btn btn-primary">
-                                        <em class="fa fa-arrow-alt-circle-right fs-5 me-2 icon_list_menu "></em>
-                                        ไปยังหลักสูตร
-                                    </a>
-                                @endif
-                            </td>
-                        </tr>
+                        @forelse($exam_history as $key=>$item)
+                            <tr>
+                                <td>{{ $key + 1 }}</td>
+                                <td>{{ $item->curriculum->name }}</td>
+                                <td class="text-center">
+                                    @if (!$item->post_date_finished)
+                                        <span class="text-default">ยังไม่จบบทเรียน</span>
+                                    @elseif($item->post_pass_status == 'y')
+                                        <span class="text-success">ผ่าน</span>
+                                    @else
+                                        <span class="text-danger">ไม่ผ่าน</span>
+                                    @endif
+                                </td>
+                                <td>
+                                    {{ DbToDate($item->post_date_finished, true, true) }}
+                                </td>
+                                <td class="text-center">
+                                    @if ($item->post_pass_status == 'y')
+                                        <a href="{{ url('certificate/pdf/' . $item->curriculum_id) }}" class="btn btn-success"><i class="fa fa-download"></i> ดาวน์โหลดใบประกาศ</a>
+                                    @else
+                                        <a href="{{ url('elearning/curriculum/' . $item->curriculum_id) }}" class="btn btn-primary">
+                                            <em class="fa fa-arrow-alt-circle-right fs-5 me-2 icon_list_menu "></em>
+                                            ไปยังหลักสูตร
+                                        </a>
+                                    @endif
+                                </td>
+                            </tr>
                         @empty
                         @endempty
-                    </tbody>
-                </table>
-            </div>
-                
+                </tbody>
+            </table>
         </div>
-    </div>    
+
+    </div>
+</div>
 @endsection
 
 @push('js')
-    @if (session('success'))
-        <script>
-            Swal.fire({
-                title: 'สำเร็จ!',
-                text: '{{ session('success') }}',
-                icon: 'success',
-                confirmButtonText: 'ตกลง'
-            });
-        </script>
-    @endif
+@if (session('success'))
+    <script>
+        Swal.fire({
+            title: 'สำเร็จ!',
+            text: '{{ session('success') }}',
+            icon: 'success',
+            confirmButtonText: 'ตกลง'
+        });
+    </script>
+@endif
 @endpush
--- resources/views/frontend/elearning/index.blade.php
+++ resources/views/frontend/elearning/index.blade.php
@@ -3,54 +3,54 @@
 @section('page', 'ระบบการเรียนรู้ออนไลน์ e-learning')
 
 @section('content')
-    <div class="container">        
+    <div class="container">
         <div class="row g-4 justify-content-between mb-5 wow fadeInDown">
             <div class="col-lg-6">
                 <div class="position-relative pb-2 my-4">
-                    <img src="http://localhost/html/images/icon-hat.svg" alt="" class="icon-hat">
+                    <img src="{{ asset('html/images/icon-hat.svg') }}" alt="" class="icon-hat">
                     <div class="title-send-us">เลือกหลักสูตรที่ท่านต้องการเรียนรู้</div>
                 </div>
-            </div>            
-        </div>        
+            </div>
+        </div>
 
         <div class="row row-cols-1 row-cols-md-2 row-cols-lg-2 g-4 justify-content-center wow fadeInDown" style="visibility: visible; animation-name: fadeInDown;">
             @forelse($curriculum as $item)
-            @php
-                $url = url('elearning/curriculum/'.$item->id);
-            @endphp
+                @php
+                    $url = url('elearning/curriculum/' . $item->id);
+                @endphp
                 <div class="col my-6">
                     <div class="blog-style1">
                         <div class="blog-img">
-                            <a href="{{$url}}">
-                                <img src="{{ Storage::url('uploads/curriculum/' . @$item->cover_image) }}" alt="{{$item->name}}" class="img-fluid w-100">                                
+                            <a href="{{ $url }}">
+                                <img src="{{ Storage::url('uploads/curriculum/' . @$item->cover_image) }}" alt="{{ $item->name }}" class="img-fluid w-100">
                             </a>
                         </div>
                         <div class="blog-content">
-                            <span class="lesson"><img src="{{url('html/images/bulb.svg')}}" alt="" width="24">{{$item->name}}</span>
+                            <span class="lesson"><img src="{{ url('html/images/bulb.svg') }}" alt="" width="24">{{ $item->name }}</span>
                             <h3 class="blog-title h5">
-                                <a href="{{$url}}">{!!$item->intro!!}</a>
+                                <a href="{{ $url }}">{!! $item->intro !!}</a>
                             </h3>
-                            <p>มีทั้งหมด {{@$item->curriculum_lesson->count()}} หน่วยการเรียนรู้/บทเรียน</p>
+                            <p>มีทั้งหมด {{ @$item->curriculum_lesson->count() }} หน่วยการเรียนรู้/บทเรียน</p>
                         </div>
-                        <a href="{{$url}}" class="blog-btn text-center">ดูรายละเอียด</a>
+                        <a href="{{ $url }}" class="blog-btn text-center">ดูรายละเอียด</a>
                     </div>
-                </div>     
+                </div>
             @empty
-                
-            @endempty       
-        </div>
-    </div>    
+
+            @endempty
+    </div>
+</div>
 @endsection
 
 @push('js')
-    @if (session('success'))
-        <script>
-            Swal.fire({
-                title: 'สำเร็จ!',
-                text: '{{ session('success') }}',
-                icon: 'success',
-                confirmButtonText: 'ตกลง'
-            });
-        </script>
-    @endif
+@if (session('success'))
+    <script>
+        Swal.fire({
+            title: 'สำเร็จ!',
+            text: '{{ session('success') }}',
+            icon: 'success',
+            confirmButtonText: 'ตกลง'
+        });
+    </script>
+@endif
 @endpush
