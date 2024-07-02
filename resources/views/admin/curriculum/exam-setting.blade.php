8c508fdd03ac5e65f232a19e5148faaa8edd16db
unisexx <unisexx@gmail.com>
2024-07-02 14:20:03 +0700

    updated

--- resources/views/admin/curriculum/exam-setting.blade.php
+++ resources/views/admin/curriculum/exam-setting.blade.php
@@ -3,26 +3,26 @@
 @section('title', 'ข้อมูลหลักสูตร / กำหนดค่าการทำแบบทดสอบ')
 
 @section('content')
-@php
-    $sum_prepost_question = 0;
-@endphp
-{!! Form::open([
-    'route' => 'admin.curriculum-exam-setting.store',
-    'method' => 'POST',
-    'files' => true,
-    'class' => 'form',
-    'autocomplete' => 'off',
-]) !!}
+    @php
+        $sum_prepost_question = 0;
+    @endphp
+    {!! Form::open([
+        'route' => 'admin.curriculum-exam-setting.store',
+        'method' => 'POST',
+        'files' => true,
+        'class' => 'form',
+        'autocomplete' => 'off',
+    ]) !!}
     <div class="card mb-3">
         <div class="card-header pb-0">
             <div class="col-12">
                 <h5>หลักสูตร : <a href=""><span class="text-success">{{ $curriculum->name }}</span></a></h5>
-                <input type="hidden" name="curriculum_id" value="{{$curriculum->id}}">
+                <input type="hidden" name="curriculum_id" value="{{ $curriculum->id }}">
             </div>
             <div class="d-flex align-items-center">
-                <div>                    
-                    <h5 class="mb-0">กำหนดค่าการทำแบบทดสอบ</h5>                    
-                </div>                
+                <div>
+                    <h5 class="mb-0">กำหนดค่าการทำแบบทดสอบ</h5>
+                </div>
             </div>
         </div>
         <div class="card-body p-3">
@@ -31,12 +31,12 @@
                     <thead>
                         <tr>
                             <th width="50" scope="col">#</th>
-                            <th width="100">ชื่อหน่วยการเรียนรู้/บทเรียน</th>                                                        
+                            <th width="100">ชื่อหน่วยการเรียนรู้/บทเรียน</th>
                             <th width="100" class="text-center" scope="col">
                                 จำนวนข้อ
                                 <br>แบบทดสอบท้ายบท
                                 <br>ที่เปิดใช้งาน
-                            </th>                                                        
+                            </th>
                             <th width="100" class="text-center" scope="col">เปิดให้ทดสอบ</th>
                             <th width="100" class="text-center" scope="col">การแสดง<br>คำถาม</th>
                             {{-- <th width="100" class="text-center" scope="col">การแสดง<br>คำตอบ</th> --}}
@@ -47,41 +47,44 @@
                     </thead>
                     <tbody>
                         @foreach ($curriculum->curriculum_lesson as $item)
-                        @php
-                            if(!empty($rs)){
-                                $detail = $rs->curriculum_exam_setting_detail()->where('curriculum_lesson_id',$item->id)->first();
-                                $exam_status = $detail->exam_status;
-                                $question_random_status = $detail->question_random_status;
-                                $n_question = $detail->n_question;
-                                $pass_score = $detail->pass_score;
-                                $n_prepost_lesson_question = $detail->n_prepost_lesson_question;
-                                $sum_prepost_question+=$n_prepost_lesson_question;
-                            }else{
-                                $exam_status = '';
-                                $question_random_status = '';
-                                $n_question = 0;
-                                $pass_score = 0;
-                                $n_prepost_lesson_question = 0;
-                                $sum_prepost_question+=$n_prepost_lesson_question;
-                            }
-                        @endphp
+                            @php
+                                if (!empty($rs)) {
+                                    $detail = $rs
+                                        ->curriculum_exam_setting_detail()
+                                        ->where('curriculum_lesson_id', $item->id)
+                                        ->first();
+                                    $exam_status = $detail->exam_status;
+                                    $question_random_status = $detail->question_random_status;
+                                    $n_question = $detail->n_question;
+                                    $pass_score = $detail->pass_score;
+                                    $n_prepost_lesson_question = $detail->n_prepost_lesson_question;
+                                    $sum_prepost_question += $n_prepost_lesson_question;
+                                } else {
+                                    $exam_status = '';
+                                    $question_random_status = '';
+                                    $n_question = 0;
+                                    $pass_score = 0;
+                                    $n_prepost_lesson_question = 0;
+                                    $sum_prepost_question += $n_prepost_lesson_question;
+                                }
+                            @endphp
                             <tr>
-                                <td>{{ $loop->iteration }}</td>                                
-                                <td style="white-space: break-spaces;">{{ @$item->name }}</td>                                
-                                <td class="text-center">{{ @$item->curriculum_lesson_question->where('status','active')->count().'/'.@$item->curriculum_lesson_question->count() }}</td>
+                                <td>{{ $loop->iteration }}</td>
+                                <td style="white-space: break-spaces;">{{ @$item->name }}</td>
+                                <td class="text-center">{{ @$item->curriculum_lesson_question->where('status', 'active')->count() . '/' . @$item->curriculum_lesson_question->count() }}</td>
                                 <td class="text-center">
                                     <div class="form-switch" style="display:block;">
-                                        {!! Form::hidden('exam_status_'.$item->id, 'inactive') !!}
-                                        {!! Form::checkbox('exam_status_'.$item->id, 'active', @$exam_status == 'active' ? true : false, ['class' => 'form-check-input exam_status']) !!}
+                                        {!! Form::hidden('exam_status_' . $item->id, 'inactive') !!}
+                                        {!! Form::checkbox('exam_status_' . $item->id, 'active', @$exam_status == 'active' ? true : false, ['class' => 'form-check-input exam_status']) !!}
                                         &nbsp;เปิด
                                     </div>
                                 </td>
                                 <td class="text-center">
                                     <div class="form-switch" style="display:block;">
-                                        {!! Form::hidden('question_random_status_'.$item->id, 'inactive') !!}
-                                        {!! Form::checkbox('question_random_status_'.$item->id, 'active', @$question_random_status == 'active' ? true : false, ['class' => 'form-check-input check-question']) !!}
+                                        {!! Form::hidden('question_random_status_' . $item->id, 'inactive') !!}
+                                        {!! Form::checkbox('question_random_status_' . $item->id, 'active', @$question_random_status == 'active' ? true : false, ['class' => 'form-check-input check-question']) !!}
                                         &nbsp;สุ่ม
-                                    </div>                                                                     
+                                    </div>
                                 </td>
                                 {{-- <td class="text-center">
                                     <div class="custom-control custom-checkbox">
@@ -90,14 +93,14 @@
                                     </div>                                    
                                 </td> --}}
                                 <td class="text-center">
-                                    <input type="number" style="" class="form-control text-center input-question" name="n_question_{{$item->id}}" value="{{$n_question}}">                                   
+                                    <input type="number" style="" class="form-control text-center input-question" name="n_question_{{ $item->id }}" value="{{ $n_question }}">
                                 </td>
                                 <td class="text-center">
-                                    <input type="number" style="" class="form-control text-center input-score" name="pass_score_{{$item->id}}" value="{{$pass_score}}">                                   
+                                    <input type="number" style="" class="form-control text-center input-score" name="pass_score_{{ $item->id }}" value="{{ $pass_score }}">
                                 </td>
                                 <td class="text-center">
-                                    <input type="number" style="" class="form-control text-center input-score input-prepost-question" name="n_prepost_lesson_question_{{$item->id}}" value="{{$n_prepost_lesson_question}}">                                   
-                                </td>                                
+                                    <input type="number" style="" class="form-control text-center input-score input-prepost-question" name="n_prepost_lesson_question_{{ $item->id }}" value="{{ $n_prepost_lesson_question }}">
+                                </td>
                             </tr>
                         @endforeach
                     </tbody>
@@ -105,14 +108,14 @@
                         <tr>
                             <th class="text-start" colspan="9">
                                 สรุปจำนวนข้อคำถามที่แสดงใน Pre/PostTest
-                                <span id="sp_sum_prepost_question" style="color:blue;padding-left:15px;padding-right:15px;">{{@$sum_prepost_question}}</span>
+                                <span id="sp_sum_prepost_question" style="color:blue;padding-left:15px;padding-right:15px;">{{ @$sum_prepost_question }}</span>
                                 ข้อ
                             </th>
                         </tr>
                         <tr>
                             <th class="text-start" colspan="9">
-                                ระบุคะแนนขั้นต่ำเพื่อผ่านการทดสอบ Pre/Post Test                                
-                                <input type="number" name="prepost_pass_score" class="form-control text-center" style="max-width:200px;" value="{{@$rs->prepost_pass_score}}">
+                                ระบุคะแนนขั้นต่ำเพื่อผ่านการทดสอบ Pre/Post Test
+                                <input type="number" name="prepost_pass_score" class="form-control text-center" style="max-width:200px;" value="{{ @$rs->prepost_pass_score }}">
                             </th>
                         </tr>
                         <tr>
@@ -123,74 +126,70 @@
                                         <div class="form-switch" style="display:block;">
                                             {!! Form::hidden('pre_test_status', 'inactive') !!}
                                             {!! Form::checkbox('pre_test_status', 'active', @$rs->pre_test_status == 'active' ? true : false, ['class' => 'form-check-input check-question']) !!}
-                                        </div>     
+                                        </div>
                                     </div>
                                     <div>
                                         เปิดให้ทำ Post-Test
                                         <div class="form-switch" style="display:block;">
                                             {!! Form::hidden('post_test_status', 'inactive') !!}
                                             {!! Form::checkbox('post_test_status', 'active', @$rs->post_test_status == 'active' ? true : false, ['class' => 'form-check-input check-question']) !!}
-                                        </div>     
+                                        </div>
                                     </div>
                                     <div style="margin-left:30px;">
                                         เปิดให้ทำ Post-Test ได้หลังจากที่ทำ Pre Test แล้ว
                                         <div class="form-switch" style="display:block;">
                                             {!! Form::hidden('post_test_after_pre', 'inactive') !!}
                                             {!! Form::checkbox('post_test_after_pre', 'active', @$rs->post_test_after_pre == 'active' ? true : false, ['class' => 'form-check-input check-question']) !!}
-                                        </div>     
+                                        </div>
                                     </div>
                                 </div>
                             </th>
                         </tr>
                     </tfoot>
                 </table>
-            </div>   
-        
-            <div class="col-auto mt-3">
-                <div class="float-start">
-                    {{ link_to_route('admin.curriculum.index', 'ย้อนกลับ', $parameters = [], $attributes = ['class' => 'btn btn-lg btn-light px-4']) }}
-                </div>
-                <div class="float-end">
-                    {{ Form::submit('บันทึก', ['class' => 'btn btn-lg bg-gradient-primary']) }}
-                </div>
+            </div>
+
+            <div class="col-auto mt-3 float-end">
+                {{ link_to_route('admin.curriculum.index', 'ย้อนกลับ', $parameters = [], $attributes = ['class' => 'btn btn-lg btn-light px-4']) }}
+                {{ Form::submit('บันทึก', ['class' => 'btn btn-lg bg-gradient-primary']) }}
             </div>
         </div>
-    </div>    
-{!! Form::close() !!}
+    </div>
+    {!! Form::close() !!}
 @endsection
 @push('js')
-<script>
-    $(document).ready(function(){       
+    <script>
+        $(document).ready(function() {
 
-        function checkExamEnable(element){
-            if(element.prop('checked')){           
-                element.closest('tr').find('.check-question, .check-answer, .input-question, .input-score').each(function(){     
-                    $(this).removeAttr('disabled'); 
-                });
-            }else{
-                element.closest('tr').find('.check-question, .check-answer, .input-question, .input-score').each(function(){
-                    $(this).prop('checked', false);
-                    $(this).attr('disabled','disabled');  
-                });
+            function checkExamEnable(element) {
+                if (element.prop('checked')) {
+                    element.closest('tr').find('.check-question, .check-answer, .input-question, .input-score').each(function() {
+                        $(this).removeAttr('disabled');
+                    });
+                } else {
+                    element.closest('tr').find('.check-question, .check-answer, .input-question, .input-score').each(function() {
+                        $(this).prop('checked', false);
+                        $(this).attr('disabled', 'disabled');
+                    });
+                }
             }
-        }
 
-        $(".exam_status").each(function(){
-            checkExamEnable($(this));
-        });
-
-        $(".input-prepost-question").keyup(function(){
-            var n_question = 0;
-            $(".input-prepost-question").each(function(){
-                
-                n_question+= $(this).val() != '' ? parseInt($(this).val()) : 0;                
+            $(".exam_status").each(function() {
+                checkExamEnable($(this));
             });
-            $("#sp_sum_prepost_question").html(n_question);
-        });
 
-        $(".exam_status").click(function(){
-            checkExamEnable($(this));
+            $(".input-prepost-question").keyup(function() {
+                var n_question = 0;
+                $(".input-prepost-question").each(function() {
+
+                    n_question += $(this).val() != '' ? parseInt($(this).val()) : 0;
+                });
+                $("#sp_sum_prepost_question").html(n_question);
+            });
+
+            $(".exam_status").click(function() {
+                checkExamEnable($(this));
+            });
         });
-    });
-</script>
+    </script>
 @endpush
