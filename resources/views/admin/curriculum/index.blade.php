5b713fdeaf3c175dd6029516405e698f1fd3ea3f
unisexx <unisexx@gmail.com>
2024-07-02 11:46:35 +0700

    updated

--- resources/views/admin/curriculum/index.blade.php
+++ resources/views/admin/curriculum/index.blade.php
@@ -14,8 +14,8 @@
                         <tr>
                             <th width="50" class="text-center" scope="col">#</th>
                             <th width="200" scope="col">ภาพหน้าปก</th>
-                            <th class="text-start" scope="col">ชื่อหลักสูตร</th>                            
-                            <th class="text-center" scope="col">ประเภทสมาชิก</th> 
+                            <th class="text-start" scope="col">ชื่อหลักสูตร</th>
+                            <th class="text-center" scope="col">ประเภทสมาชิก</th>
                             <th width="100" class="text-center" scope="col">เปิดใช้งาน</th>
                             <th width="200" class="text-center" scope="col">จัดการ</th>
                         </tr>
@@ -23,14 +23,14 @@
                     <tbody>
                         @foreach ($rs as $item)
                             <tr>
-                                <td class="text-center">{{ autoNumber($rs) }}</td>        
-                                <td><img src="{{ Storage::url('uploads/curriculum/' . @$item->cover_image) }}" height="250"></td>          
+                                <td class="text-center">{{ autoNumber($rs) }}</td>
+                                <td><img src="{{ Storage::url('uploads/curriculum/' . @$item->cover_image) }}" height="150"></td>
                                 <td style="vertical-align:top;padding-left:20px;">{{ @$item->name }}</td>
                                 <td style="vertical-align:top;padding-left:20px;">
                                     @php
-                                        if($item->curriculum_user_type()->count() > 0){
-                                            foreach($item->curriculum_user_type()->get() as $ut){
-                                                echo '<div>-'.$ut->user_type->name.'</div>';
+                                        if ($item->curriculum_user_type()->count() > 0) {
+                                            foreach ($item->curriculum_user_type()->get() as $ut) {
+                                                echo '<div>-' . $ut->user_type->name . '</div>';
                                             }
                                         }
                                     @endphp
@@ -42,10 +42,10 @@
                                             {{ @$item->id }}
                                         @endslot
                                     @endcomponent
-                                    <a href="{{ url('admin/curriculum-lesson?curriculum_id='.$item->id) }}" class="mx-3">
+                                    <a href="{{ url('admin/curriculum-lesson?curriculum_id=' . $item->id) }}" class="mx-3">
                                         <i class="fas fa-book text-secondary" aria-hidden="true" data-toggle="tooltip" data-placement="top" title="จัดการบทเรียนและแบบทดสอบท้ายบทเรียน"></i>
                                     </a>
-                                    <a href="{{ url('admin/curriculum-exam-setting?curriculum_id='.$item->id) }}" class="">
+                                    <a href="{{ url('admin/curriculum-exam-setting?curriculum_id=' . $item->id) }}" class="">
                                         <i class="fas fa-cog text-secondary" aria-hidden="true" data-toggle="tooltip" data-placement="top" title="กำหนดการการทำแบบทดสอบ"></i>
                                     </a>
                                 </td>
@@ -55,8 +55,8 @@
                 </table>
             </div>
             <!-- Pagination -->
-            <div class="mt-3">                
-                {{ $rs->appends(@$_GET)->render("vendor.pagination.bootstrap-5-right") }}
+            <div class="mt-3">
+                {{ $rs->appends(@$_GET)->render('vendor.pagination.bootstrap-5-right') }}
             </div>
         @endslot
     @endcomponent
