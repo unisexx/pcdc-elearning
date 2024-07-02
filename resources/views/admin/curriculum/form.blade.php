ec6541047ef411303d922f9d8aca78bb640f4453
unisexx <unisexx@gmail.com>
2024-07-02 21:52:28 +0700

    updated

--- resources/views/admin/curriculum/form.blade.php
+++ resources/views/admin/curriculum/form.blade.php
@@ -4,7 +4,7 @@
     @endslot
 
     @slot('form')
-        <div class="row mt-3">            
+        <div class="row mt-3">
             <div class="col-12">
                 {{ Form::bsText('name', 'ชื่อหลักสูตร') }}
             </div>
@@ -16,7 +16,7 @@
             </div>
             <div class="col-12">
                 {{ Form::bsTiny('pass_criteria', 'เกณฑ์การผ่านหลักสูตร') }}
-            </div>            
+            </div>
             <div class="col-12">
                 {{ Form::bsFile('cover_image', 'ภาพหน้าปก (ขนาด 1400 x 476 px)', @$rs->cover_image, 'uploads/curriculum', ['accept' => 'image/*']) }}
             </div>
@@ -24,19 +24,24 @@
                 <label>ประเภทสมาชิกที่สามารถเข้าเรียน</label>
                 <div>
                     @php
-                        $user_type = \App\Models\UserType::orderBy('pos','asc')->get();   
+                        $user_type = \App\Models\UserType::orderBy('pos', 'asc')->get();
                     @endphp
-                    @foreach($user_type as $key=>$ut)                  
+                    @foreach ($user_type as $key => $ut)
                         @php
-                            if(@$rs){
-                                $checked = $rs->curriculum_user_type()->where('user_type_id',$ut->id)->count() ? true : false;
-                            }else{
+                            if (@$rs) {
+                                $checked = $rs
+                                    ->curriculum_user_type()
+                                    ->where('user_type_id', $ut->id)
+                                    ->count()
+                                    ? true
+                                    : false;
+                            } else {
                                 $checked = false;
                             }
-                        @endphp  
+                        @endphp
                         <label>
-                            {!! Form::checkbox('user_type_id[]', $ut->id, @$checked, ['class' => 'form-check-input ms-2', 'style'=>'border:1px solid #CCC;']) !!}
-                            {{$ut->name}}                    
+                            {!! Form::checkbox('user_type_id[]', $ut->id, @$checked, ['class' => 'form-check-input ms-2', 'style' => 'border:1px solid #CCC;']) !!}
+                            {{ $ut->name }}
                         </label>
                     @endforeach
                 </div>
@@ -46,13 +51,9 @@
             </div>
         </div>
 
-        <div class="col-auto mt-3">
-            <div class="float-start">
-                {{ link_to_route('admin.curriculum.index', 'ย้อนกลับ', $parameters = [], $attributes = ['class' => 'btn btn-lg btn-light px-4']) }}
-            </div>
-            <div class="float-end">
-                {{ Form::submit('บันทึก', ['class' => 'btn btn-lg bg-gradient-primary']) }}
-            </div>
+        <div class="col-auto mt-3 float-end">
+            {{ link_to_route('admin.curriculum.index', 'ย้อนกลับ', $parameters = [], $attributes = ['class' => 'btn btn-lg btn-light px-4']) }}
+            {{ Form::submit('บันทึก', ['class' => 'btn btn-lg bg-gradient-primary']) }}
         </div>
     @endslot
 @endcomponent
